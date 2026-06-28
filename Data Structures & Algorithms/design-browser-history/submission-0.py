b90a848970b365f75eeca48deabd9eeeae8f09eb
Gemma Qu <gemma.qu@duke.edu>
2026-06-28 22:33:05 +0800

    Add: design-browser-history - submission-0

---
+++ Data Structures & Algorithms/design-browser-history/submission-0.py
@@ -0,0 +1,37 @@
+class ListNode:    
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class BrowserHistory:
+
+    def __init__(self, homepage: str):
+        self.history = [homepage]
+        self.current = 0
+        
+
+    def visit(self, url: str) -> None:
+        self.history = self.history[:self.current+1] #get a slice, not remove
+        self.history.append(url) 
+        self.current += 1 #move current!
+        #or we could write this as
+        # self.current = len(self.history)-1
+        
+
+    def back(self, steps: int) -> str:
+        self.current = max(0,self.current-steps)
+        return self.history[self.current]
+
+        
+
+    def forward(self, steps: int) -> str:
+        self.current = min(self.current+steps, len(self.history)-1)
+        return self.history[self.current]
+        
+
+
+# Your BrowserHistory object will be instantiated and called as such:
+# obj = BrowserHistory(homepage)
+# obj.visit(url)
+# param_2 = obj.back(steps)
+# param_3 = obj.forward(steps)
